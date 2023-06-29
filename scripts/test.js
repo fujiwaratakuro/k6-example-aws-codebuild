f854a2d268f4c0919831e7d4ab21821113ab1b7a
ふじわらたくろう <84885060+fujiwaratakuro@users.noreply.github.com>
2023-06-29 19:04:45 +0900

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,10 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const VUS = 100;
+const DURATION = "1s";
+
 export default function () {
-    const res = http.get('https://fujiwarawarp.com/wordpress/');
+    const res = http.get('http://home.takuro.work/');
     sleep(1);
 }
