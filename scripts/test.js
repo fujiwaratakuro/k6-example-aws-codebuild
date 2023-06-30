cfb4b8e761f249eb14cc183b1963ccb2d2a9ed39
ふじわらたくろう <84885060+fujiwaratakuro@users.noreply.github.com>
2023-06-30 10:35:48 +0900

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,11 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-
+export const options = {
+  vus: 10000,
+  duration: '20s',
+  iterations: 6,
+};
 
 export default function () {
     const res = http.get('http://home.takuro.work/');
