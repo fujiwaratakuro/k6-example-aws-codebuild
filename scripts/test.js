ce55edf1df0aa33acd3409ab77d2c3fe3040eba8
ふじわらたくろう <84885060+fujiwaratakuro@users.noreply.github.com>
2023-07-06 14:57:39 +0900

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -2,9 +2,9 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 10000,
+  vus: 10,
   duration: '120s',
-//  iterations: 1,
+  iterations: 1200,
 };
 
 export default function () {
